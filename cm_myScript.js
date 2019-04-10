2d9fdd0667ab5bc9a9c4d928fc83a45582b67aa5
akanekoh <48981086+akanekoh@users.noreply.github.com>
2019-04-10 16:01:56 +0900

    Update cm_myScript.js

--- cm_myScript.js
+++ cm_myScript.js
@@ -1,25 +1,59 @@
 
-//var ルート, 1 ユーロ = 125.05 日本円。
-var jpyRate =125.05;
-//var ルート, 1 ユーロ = 34.76 台湾ドール。
-var twdRate =34.8;
 
-//function EurToJpy(), ユーロから日本円にの計算。
-function eurToJpy(price){
-    return price * jpyRate;
+// var jpyRate =125.05;
+// var twdRate =34.8;
+
+//1 ユーロ = 125.05 日本円。
+function eurToJpy(eurPrice){
+    return eurPrice * 125.05;
 } 
 
-function eurToTwd(price){
-    return price * twdRate;
+//1 ユーロ = 34.76 台湾ドール。
+function eurToTwd(eurPrice){
+    return eurPrice * 34.8;
 } 
 
-function printPrice(){
-    var price = document.getElementById("price").value;
-    x=eurToJpy(price);
-    y=eurToTwd(price);
-    document.getElementById("jpyPrice").innerHTML=Math.round(x);
-    document.getElementById("twdPrice").innerHTML=Math.round(y);
+function changeEurPrice(){
+    var eurPrice = document.getElementById("eurPriceIn").value;
+    jpy=eurToJpy(eurPrice);
+    twd=eurToTwd(eurPrice);
+    document.getElementById("jpyPriceOutE").innerHTML=Math.round(jpy);
+    document.getElementById("twdPriceOutE").innerHTML=Math.round(twd);
+}
+
+//1 日本円 = 0.0080 ユーロ。
+function jpyToEur(jpyPrice){
+    return jpyPrice * 0.0080;
+} 
+
+//1 日本円 = 0.28 台湾ドール。
+function jpyToTwd(jpyPrice){
+    return jpyPrice * 0.28;
+} 
+
+function changeJpyPrice(){
+    var jpyPrice = document.getElementById("jpyPriceIn").value;
+    eur=jpyToEur(jpyPrice);
+    twd=jpyToTwd(jpyPrice);
+    document.getElementById("eurPriceOutJ").innerHTML=eur;
+    document.getElementById("twdPriceOutJ").innerHTML=Math.round(twd);
 }
 
 
-　
+//1 台湾ドール = 0.029 ユーロ。
+function twdToEur(twdPrice){
+    return twdPrice * 0.029;
+} 
+
+//1 台湾ドール = 3.60 日本円。
+function twdToJpy(twdPrice){
+    return twdPrice * 3.60;
+} 
+
+function changeTwdPrice(){
+    var twdPrice = document.getElementById("twdPriceIn").value;
+    eur=twdToEur(twdPrice);
+    jpy=twdToJpy(twdPrice);
+    document.getElementById("eurPriceOutT").innerHTML=eur;
+    document.getElementById("jpyPriceOutT").innerHTML=Math.round(jpy);
+}
